2d135fd9385df08874611ab7b586d631db1ab7dc
tomi084 <tomiolmedo84@gmail.com>
2024-08-24 15:11:21 -0300

    generador.cpp
    Realizar transacciones

--- generador.cpp
+++ generador.cpp
@@ -5,60 +5,120 @@
 
 using namespace std;
 
-struct Transaccion {
-	double monto;
-	int fecha;
-	string id;
-	string tipo;//egreso o ingreso
-};
-
 struct Cliente {
     string nombre;
     string dni;
     string username;
     string clave;
-    double saldo;
-    Transaccion transacciones;
+    int saldo;
 };
 
-bool iniciarSesion(Cliente &clientes){
+struct Transaccion {
+    int id;
+    string username;
+    int fecha; // formato "aaaa/mm/dd"
+    int monto;
+    string tipo; // Egreso o Ingreso
+};
+
+
+// Declaracion de funciones
+void realizarTransaccion(Transaccion &transaccion, Cliente &cliente);
+Cliente buscarCliente(Cliente &cliente, string username, string clave);
+void registrarTransaccion (Transaccion &transaccion, Cliente &cliente, string username, int monto, string tipoTransaccion, int fecha);
+
+
+// Realizacion completa de la transaccion
+void realizarTransaccion(Transaccion &transaccion, Cliente &cliente){
 	string username, clave;
+	cout << "Ingrese su username: ";
+	cin >> username;
 	
-	cout<< "Username: ";
-	cin>> username;
+	cout << "Ingrese su clave: ";
+	cin >> clave;
 	
-	cout<< "clave: ";
-	cin>> clave;
-	
-	FILE* archivo = fopen("Clientes.txt", "rb");
-	if(archivo !=NULL){
-		while(fread(&clientes, sizeof(Cliente), 1, archivo)==1){
-			if(clientes.username== username && clientes.clave== clave){
-				return true;
-			}	
+	// Buscar si el cliente ingresado está en el sistema
+	cliente = buscarCliente(cliente, username, clave);
+	if (cliente.saldo == -1) {
+		cout << "Username o clave incorrectos.\n";
+	} else {
+		cout << "\nBienvenido/a " << cliente.nombre << ". Saldo actual: $" << cliente.saldo << endl;
+		
+		// Si el cliente se encuentra en el sistema, se procede con la transaccion
+		int monto, fecha;
+		string tipoTransaccion;
+		cout << "Ingrese monto: ";
+		cin >> monto;
+		cout << "Escriba tipo de transaccion: EGRESO o INGRESO: ";
+		cin >> tipoTransaccion;
+		cout << "Ingrese fecha (aaaa/mm/dd): ";
+		cin >> fecha;		
+		
+		if (tipoTransaccion != "EGRESO" && tipoTransaccion != "INGRESO"){
+			cout << "Error: tipo de transaccion incorrecto.\n";
+		} else {
+			// Funcion para registrar correctamente la transaccion en el sistema
+			registrarTransaccion(transaccion, cliente, username, monto, tipoTransaccion, fecha);
+			cout << "Transaccion realizada con exito. Saldo actual: $" << cliente.saldo << endl;
 		}
 	}
-	return false;
 }
-/*
-void agregarTransaccion(Cliente &clientes){
-	Transaccion transacciones;
-	int opcion, fecha;
+
+// Funcion para buscar si el cliente ingresado se encuentra en el sistema
+Cliente buscarCliente(Cliente &cliente, string username, string clave){
+	FILE* archivoClientes = fopen("Clientes.txt", "r");
 	
-	cout << "Ingrese monto: ";
-	cin >> transacciones.monto;
-	
-	cout << "1. Egreso de dinero\n2. Ingreso de dinero\nEscriba una opcion: ";
-	cin >> opcion;
-	
-	cout << "Ingrese fecha (ddmmaaa): ";
-	cin >> transacciones.fecha;
-	
-	FILE* archivo = fopen("Clientes.txt", "a");
-	if(archivo != NULL){
-		fwrite(&transacciones, sizeof(Cliente), 1, archivo);
-		fclose(archivo);
+	while (fread(&cliente, sizeof(Cliente), 1, archivoClientes)){
+		if ((cliente.username == username) && (cliente.clave == clave)){
+			fclose(archivoClientes);
+			return cliente;
+		}
 	}
 	
+	fclose(archivoClientes);
+	Cliente clienteVacio = {"","","","", -1};
+	return clienteVacio;
 }
-*/
+
+
+// Funcion para registrar correctamente la transaccion dentro del sistema
+void registrarTransaccion(Transaccion &transaccion, Cliente &cliente, string username, int monto, string tipoTransaccion, int fecha){
+	int ultimoID=0;
+	FILE* archivoTransaccionesLectura = fopen("Transacciones.txt", "rb");
+	
+	// Encontrar el ultimo ID registrado
+	while (fread(&transaccion, sizeof(Transaccion), 1, archivoTransaccionesLectura)){
+		ultimoID = transaccion.id;
+	}
+	fclose(archivoTransaccionesLectura);
+	
+	// Registar la transaccion en el archivo "Transacciones.txt"
+	transaccion.id = ++ultimoID;
+	transaccion.username = username;
+	transaccion.monto = monto;
+	transaccion.tipo = tipoTransaccion;
+	transaccion.fecha = fecha;
+	
+	FILE* archivoTransacciones = fopen("Transacciones.txt", "ab");
+	fwrite(&transaccion, sizeof(Transaccion), 1, archivoTransacciones);
+	fclose(archivoTransacciones);
+	
+	// Actualizar saldo del cliente
+	FILE* archivoClientes = fopen("Clientes.txt", "rb+");
+	if (archivoClientes != NULL){
+		while (fread(&cliente, sizeof(Cliente), 1, archivoClientes)){
+			if (cliente.username == username){
+				fseek(archivoClientes, -sizeof(Cliente), SEEK_CUR);
+				if (tipoTransaccion == "INGRESO"){
+					cliente.saldo += monto;
+				} else if (tipoTransaccion == "EGRESO"){
+					cliente.saldo -= monto;
+				}
+				fwrite(&cliente, sizeof(Cliente), 1, archivoClientes);
+				break;
+			}
+		}
+		fclose(archivoClientes);
+	}
+}
+
